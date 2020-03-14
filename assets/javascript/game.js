72cec44e7360cd379f4c62a39235cb3050d9836f
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-03-14 16:43:26 -0600

    put in js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,54 @@
-var lettersGuessed = [];
-var guessesRemain = 9;
-var Wins = 0;
+var wins = 0;
 var losses = 0;
+var guessesLeft = 9;
+var userLetter = [];
+var eachofLetters = null;
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+
+window.onload = function() {
+
+    countGuessesLeft();
+
+    function countGuessesLeft() {
+        document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    }
+    
+
+}
+
+function guessesSoFar() {
+	document.querySelector("#currentGuess").innerHTML = "Your Guesses so far: " + userLetter.join(' ');
+}
+
+
+var restart = function () {
+    guessesLeft = 9;
+    userLetter = [];
+    var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+}
+
 document.onkeyup = function (event) {
+        
+        guessesLeft--;
 
+        var userGuesses = String.fromCharCode(event.keyCode).toLowerCase();
+        userLetter.push(userGuesses);
+        guessesSoFar();
 
-    
-    var userInput = event.key;
-    var ComputerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    var userOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    
-    if (userOptions.indexOf(userInput) > -1 ) {
-        if (userInput === computerOptions) {
-            Wins++;
-            document.querySelector("#numWins").innerHTML = "wins" + numWins;
-            guessesRemain = 9;
-            lettersGuessed = [];
+        
+    if (userGuesses === computerGuess) {
+            wins++;
+            document.querySelector("#wins").innerHTML = "Wins:" + wins;
+            restart();
         }
-        else if (guessesRemain === 0) {
+    else if (guessesLeft === 0) {
             losses++;
-            document.querySelector("#Losses").innerHTML = "Wins" + numWins;
-            lettersGuessed = [];
+            document.querySelector("#losses").innerHTML = "Losses:" + losses;
+            restart();
         }
        
-    }
-};
+    };
+
 
